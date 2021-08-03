304d144fd61478c30f38a76bb69a195646dfb0bc
TUSHAR SHARMA <tusharsparsh23@gmail.com>
2021-08-03 13:15:23 +0530

    updated fandom quiz

--- fandomquiz.js
+++ fandomquiz.js
@@ -1,9 +1,10 @@
 var requirement = require("readline-sync");
 currentScore = 0;
+
+console.log(`---HI THIS IS FANDOM QUIZ !!TRY YOUR LUCK`);
 function checkAnswer(myquestion, myanswer) {
-  var newName = requirement.question(myquestion.UpperCase);
-  console.log(newName);
-  if (newName === myanswer.UpperCase) {
+  var newName = requirement.question(myquestion);
+  if (newName.toLowerCase === myanswer.toLowerCase) {
     console.log("You are right");
     currentScore = currentScore + 1;
   } else {
@@ -13,32 +14,53 @@ function checkAnswer(myquestion, myanswer) {
   console.log("Current Score", +currentScore);
 }
 var object1 = {
-  question: "Your Name Please?",
-  answer: "Tushar Sharma",
+  question: `Which of them you watch most often?
+  a: Netflix
+  b:Amazon Prime
+  c:ZEE5\n`,
+  answer: "a",
 };
 var object2 = {
-  question: "You like Superheroes?",
-  answer: "yes",
+  question: `Which of them is best in thrill and suspense?
+  a:Money Hiest
+  b:Peaky Blenders
+  c:GOT\n`,
+  answer: "b",
 };
 var object3 = {
-  question: "Marvel OR DC?",
-  answer: "Marvel",
+  question: `What is name of character in PEAKY BLENDERS?
+  a:Tom
+  b:Thomas\n`,
+  answer: "b",
 };
 var object4 = {
-  question: "Your Favorite superhero?",
-  answer: "Thor",
+  question: `What is name of character of MANOJ VAJPAYEE in THE FAMILY MAN webseries?
+  a: Shubham
+  b:Arvind
+  c:Srikant\n`,
+  answer: "c",
 };
 var object5 = {
-  question: "His Weapon?",
-  answer: "Mjonir",
+  question: `On which platform THE SPECIAL OPS webseries streamed?
+  a: Netflix
+  b:Amazon Prime 
+  c:Hotstar\n`,
+  answer: "c",
 };
 var object6 = {
-  question: "His Empire",
-  answer: "Asguard",
+  question: `Which role does Robert Downey Jr. play in MARVEL movies?
+  a: Thor 
+  b:Iron Man 
+  c:SpiderMan\n `,
+  answer: "b",
 };
 var object7 = {
-  question: "Better than Thanos?",
-  answer: "No",
+  question: `Which is the best platform to enjoy watching webseries?
+  a:Netflix
+  b:Amzon Prime
+  c:Hotstar
+  d Popcorn\n`,
+  answer: "a",
 };
 var listOfObjects = [];
 listOfObjects.push(object1);
@@ -55,7 +77,7 @@ for (var i = 0; i < listOfObjects.length; i++) {
 var high = [
   {
     name: "hitesh",
-    highscore: "6",
+    highscore: "7",
   },
 ];
 for (i = 0; i < high.length; i++) {
